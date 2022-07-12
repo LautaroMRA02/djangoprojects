3cf642a4c48bca5767032744ef2b357666207dfb
Lautaro Rivadero <79938359+LautaroMRA02@users.noreply.github.com>
2022-07-12 16:57:33 -0300

    commit 001: end 1

--- djen_project/djen_project/urls.py
+++ djen_project/djen_project/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from cd_library.views import hello_word
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('test/', hello_word ),
 ]
