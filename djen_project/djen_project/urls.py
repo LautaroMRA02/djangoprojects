dc7e03c39665da5c022605c6863ab38f5c79dc8b
Lautaro Rivadero <79938359+LautaroMRA02@users.noreply.github.com>
2022-07-12 18:36:58 -0300

    commit 002: init pastebin

--- djen_project/djen_project/urls.py
+++ djen_project/djen_project/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from cd_library.views import hello_word
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('test/', hello_word ),
 ]
